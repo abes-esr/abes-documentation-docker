edeb30ba62045ef6d3223b1189cca5c2da30201f
seror <seror@abes.fr>
2024-07-09 13:57:53 +0200

    Add configs directory

---
+++ configs/documentation-gm-swish-e/scripts-swish-e/dockerExecIndexerGM.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Version 1.0
+
+docker exec documentation-gm-swish-e /opt/guide/data-swish-e/scripts/indexerGM.sh
---
+++ configs/documentation-gm-swish-e/scripts-swish-e/dockerRunGm-swish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Version 1.0
+
+docker run --restart always -d --name documentation-gm-swish-e  \
+-p 8080:80  \
+-v /var/www/html/guide/html/:/var/www/html/ \
+-v /opt/guide/data-swish-e/.swishcgi.conf:/usr/lib/cgi-bin/.swishcgi.conf \
+-v /opt/guide/data-swish-e/:/opt/guide/data-swish-e/ \
+mylocalswish-e:latest
